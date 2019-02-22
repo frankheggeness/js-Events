77d720357a8e3ad161c18c32a782b306f6e09895
unknown <fr.heggeness@gmail.com>
2019-02-22 11:58:54 -1000

    9

--- js/app.js
+++ js/app.js
@@ -142,7 +142,22 @@ function grimBio(){
 
 var messages = ["That wasn't chicken.", "The fortune you seek is in another cookie.", "Tell your friends about DevLeague Prep.", "You will be doing many events tonight.", "Don't let statistics do a number on you.", "Buy your instructor a drink and receive double fortune.", "Is a turtle without a shell naked or homeless?"];
 
+fortune.addEventListener('click', randFortune);
+
+function randFortune(){
+    var random = document.createElement('div');
+    random.id = 'newfortune';
+    var randomNumber = Math.floor(Math.random() * 8);
+    random.innerHTML = messages[randomNumber];
+    showFortune.appendChild(random);
+}
+
 
 //9. Open Sesame
 /*Add an event listener to the `secret` input element that will capitalize letter inputs in the form. Hint, you'll be using the keyup event*/
 
+secret.addEventListener('keyup', capitalize);
+
+function capitalize(){
+    secret.value = secret.value.toUpperCase()
+}
